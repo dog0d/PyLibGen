39659feb993b73ba2a09bd7fd054b8b3c4f4d785
Dog <32229085+dog0d@users.noreply.github.com>
2020-02-25 18:27:30 -0800

    updated links & formatbooks def

--- pylibgen.py
+++ pylibgen.py
@@ -15,7 +15,7 @@ def getSearchResults(term, page, column):
 	source = request.urlopen(url)
 	soup = BeautifulSoup(source, 'lxml')
 	if page == 1:
-		books_found = re.search(r'(\d+) books found', str(soup))
+		books_found = re.search(r'(\d+) files found', str(soup))
 		print(books_found.group().upper())
 		n_books = int(books_found.groups()[0])
 
